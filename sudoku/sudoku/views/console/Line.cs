4bd11d220d621096bf294074935649760be36e2d
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2021-07-21 00:34:45 +0200

    Cambiar nombre de ROW por HORIZONTAL_FIRST, quitar carácteres individuales, cambiar el método Display para que llame a ColorConsole.WriteLine

--- sudoku/sudoku/views/console/Line.cs
+++ sudoku/sudoku/views/console/Line.cs
@@ -6,34 +6,22 @@ namespace usantatecla.sudoku.views.console
     public enum Line {
         [Description("   ╔═══════════╦═══════════╦═══════════╗")]
         HORIZONTAL_FIRST,
-
-        [Description("   ║───┼───┼───║───┼───┼───║───┼───┼───║")]
+        [Description("   ║───|───|───║───|───|───║───|───|───║")]
         HORIZONTAL_SIMPLE,
-
         [Description("   ╠═══════════╬═══════════╬═══════════╣")]
         HORIZONTAL_DOUBLE,
-
         [Description("   ╚═══════════╩═══════════╩═══════════╝")]
         HORIZONTAL_LAST,
-
-        [Description("     A   B   C   D   E   F   G   H   I")]
-        HORIZONTAL_LETTERS,
-
-        [Description("|")]
-        VERTICAL_SIMPLE,
-
-        [Description("║")]
-        VERTICAL_DOUBLE,
-
+        [Description("     A   B   C   D   E   F   G   H   I  ")]
+        HORIZONTAL_LETTER,
     }
 
-    public static class LineExtension
-    {
-        public static void Display(this Line line)
-        {
-            ColorConsole.Instance().Write(line.ToString());
+    public static class LineExtensions {
+
+        public static void Display(this Line line){
+            ColorConsole.Instance().WriteLine(line.GetDescription(), System.ConsoleColor.Cyan);
         }
+
     }
 
 }
- 
